73fe6d4167e0578130cdd1d1efee55978f35aa28
TheLoganPark <Armando55441@hotmail.com>
2021-04-21 14:34:20 -0600

    Sin cambios

---
+++ app/src/main/java/com/example/eva2_2_intentos_implicitos/OtraActividad.java
@@ -0,0 +1,26 @@
+package com.example.eva2_2_intentos_implicitos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class OtraActividad extends AppCompatActivity {
+
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otra_actividad);
+
+                //Accion y Datos
+                //Actividad de Origen
+
+
+    }
+
+    public void onClick(View v){
+        startActivity(intent);
+    }
+}
